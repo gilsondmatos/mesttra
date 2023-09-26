890b5413b1a24bb26787c9c8b52f0758971f71ba
gilsondmatos <gilsondmatos@hotmail.com>
2023-09-26 10:07:38 -0300

    Exercicio 1037 e 1021

--- beecrowd/1037.js
+++ beecrowd/1037.js
@@ -3,14 +3,19 @@ var lines = input.split('\n');
 
 const valor = parseFloat(lines[0])
 
-if (valor >=0 && valor <=25) {
-    console.log("Intervalo [0,25]")
-}else if (valor > 25 && valor <= 50) {
-    console.log("Intervalo (25,50]")
-}else if (valor > 50 && valor <= 75) {
-    console.log("Intervalo (50,75]")
-}else if (valor > 75 && valor <= 100) {
-    console.log("Intervalo (75,100]")
-}else {
-    console.log('Fora de intervalo')
+function intervaloValores (valor) {
+    if (valor >=0 && valor <=25) {
+        return ("Intervalo [0,25]")
+    }else if (valor > 25 && valor <= 50) {
+        return ("Intervalo (25,50]")
+    }else if (valor > 50 && valor <= 75) {
+        return("Intervalo (50,75]")
+    }else if (valor > 75 && valor <= 100) {
+        return("Intervalo (75,100]")
+    }else {
+        return ('Fora de intervalo')
+    }
 }
+
+const intervalo = intervaloValores(valor)
+console.log(intervalo)
